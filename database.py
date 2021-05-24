71d03a9af972aa52d0889d6b5c48ec445cad65b0
yashaswinisarabu <48400728+yashaswinisarabu@users.noreply.github.com>
2021-05-24 19:31:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from datetime import date
 
 
 def db_connect():
-    _conn = MySQLdb.connect(host="127.0.0.1", user='root',
+    _conn = MySQLdb.connect(host='localhost', user='root',
                             password='root', db='speechtotext', port=3306)
     c = _conn.cursor()
 
